1ad9f27555ae12f0de234931e719a5f9205fa5a2
Rafa de Castro <rafael@micubiculo.com>
2021-12-31 00:20:38 +0100

    Not needed

--- post_template/src/app_state.rs
+++ post_template/src/app_state.rs
@@ -3,7 +3,6 @@ use crate::stateful_list::StatefulList;
 
 pub struct AppState {
     pub input: String,
-    pub cursor_position: u8,
     pub search_results: StatefulList<Book>
 }
 
@@ -11,7 +10,6 @@ impl Default for AppState {
     fn default() -> AppState {
         AppState {
             input: String::new(),
-            cursor_position: 0,
             search_results: StatefulList::with_items(vec![]),
         }
     }
